640a6bd30fb55643a1ed0b8cf8f12668364b219b
Wei <1342247033@qq.com>
2018-01-05 00:05:31 +0800

    Add extractEntity middleware get entity name from the request url

---
+++ app/Http/Middleware/ExtractEntity.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Str;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
+
+class ExtractEntity
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $parts = explode('/', $request->path());
+        if(strlen(config('rigger.api.prefix')) > 0) {
+            if(count($parts) > 1) {
+                $name = $parts[1];
+            } else {
+                throw new NotFoundResourceException();
+            }
+        } else {
+            $name = $parts[0];
+        }
+        $request['rigger_entity'] = Str::ucfirst(Str::singular($name));
+        return $next($request);
+    }
+}
